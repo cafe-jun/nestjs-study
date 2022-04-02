a4f2d1bd5eda5565f2758ad8e034b81bb5c1f3b5
Cafejun <tb25271@gmail.com>
2022-04-02 22:46:16 +0900

    RTC REdis

--- apps/rtc/src/main.ts
+++ apps/rtc/src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { RtcModule } from './rtc.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(RtcModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(RtcModule);
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+        transport: Transport.REDIS,
+        options: {
+            url: 'redis://localhost:6379',
+        },
+    });
+    await app.listen(3000);
 }
 bootstrap();
