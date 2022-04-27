bc07f465a2782604d24d1eb1b658c82b66efdc35
Cafejun <tb25271@gmail.com>
2022-04-27 23:23:42 +0900

    MonoRepo Update

--- ticket-code/src/main.ts
+++ ticket-code/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(app);
+  console.log(app, 'application app monorepo');
   await app.listen(3000);
 }
 bootstrap();
