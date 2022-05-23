3a53a67ddf9ee86016eb2113bf533cd15aa6d7ec
Cafejun <tb25271@gmail.com>
2022-05-23 22:16:03 +0900

    DatabaseCommit

--- generic-mapper/src/app.module.ts
+++ generic-mapper/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { DatabaseProvidersModule } from './module/database.module';
 import { AppService } from './app.service';
-
 @Module({
-  imports: [],
+  imports: [DatabaseProvidersModule],
   controllers: [AppController],
   providers: [AppService],
 })
