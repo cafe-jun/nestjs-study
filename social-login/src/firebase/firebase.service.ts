27c1c98a335a4d17bc22009856ac5c822dd9566e
Cafejun <tb25271@gmail.com>
2022-06-14 21:42:19 +0900

    FireBase Cmmit

--- social-login/src/firebase/firebase.service.ts
+++ social-login/src/firebase/firebase.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FireBaseService {}
+export class FireBaseService {
+  getkakaoToken() {}
+
+  getNaverToken() {}
+
+  getGoogleToken() {}
+
+  getAppleToken() {}
+}
