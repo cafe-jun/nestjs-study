bca0e6d1cb9370d9eb482a77365ca10165f26b23
Cafejun <tb25271@gmail.com>
2022-06-21 23:37:06 +0900

    CustomRepoistory

--- social-login/src/users/user.repository.ts
+++ social-login/src/users/user.repository.ts
@@ -1,1 +1,5 @@
+import { Users } from 'src/entities/user.entitiy';
+import { CustomRepository } from '../config/typeorm-ex/typeorm-ex.decorator';
+
+@CustomRepository(Users)
 export class UsersRepository {}
